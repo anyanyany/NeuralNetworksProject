8e437e20ed6c26f2064ad428865fc5322a35038d
Anna Zawadzka <a.zawadzka@op.pl>
2016-10-09 21:41:01 +0200

    some gui changes

--- Project1/NeuralNetworks/Form1.Designer.cs
+++ Project1/NeuralNetworks/Form1.Designer.cs
@@ -28,20 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.computeButton = new System.Windows.Forms.Button();
+            this.loadTrainingSetButton = new System.Windows.Forms.Button();
+            this.mainPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.loadTestSetButton = new System.Windows.Forms.Button();
+            this.defineLayersPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.deleteLayerButton = new System.Windows.Forms.Button();
+            this.addLayerButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberOfNeurons = new System.Windows.Forms.NumericUpDown();
+            this.layersList = new System.Windows.Forms.ListBox();
+            this.mainPanel.SuspendLayout();
+            this.defineLayersPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfNeurons)).BeginInit();
             this.SuspendLayout();
+            // 
+            // computeButton
+            // 
+            this.computeButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.computeButton.FlatAppearance.BorderSize = 3;
+            this.computeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.computeButton.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.computeButton.Location = new System.Drawing.Point(359, 468);
+            this.computeButton.Name = "computeButton";
+            this.computeButton.Size = new System.Drawing.Size(350, 78);
+            this.computeButton.TabIndex = 0;
+            this.computeButton.Text = "COMPUTE";
+            this.computeButton.UseVisualStyleBackColor = true;
+            this.computeButton.Click += new System.EventHandler(this.computeButton_Click);
+            // 
+            // loadTrainingSetButton
+            // 
+            this.loadTrainingSetButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.loadTrainingSetButton.FlatAppearance.BorderSize = 2;
+            this.loadTrainingSetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loadTrainingSetButton.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.loadTrainingSetButton.Location = new System.Drawing.Point(3, 3);
+            this.loadTrainingSetButton.Name = "loadTrainingSetButton";
+            this.loadTrainingSetButton.Size = new System.Drawing.Size(350, 76);
+            this.loadTrainingSetButton.TabIndex = 1;
+            this.loadTrainingSetButton.Text = "Load training set";
+            this.loadTrainingSetButton.UseVisualStyleBackColor = true;
+            this.loadTrainingSetButton.Click += new System.EventHandler(this.loadTrainingSetButton_Click);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.ColumnCount = 3;
+            this.mainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.mainPanel.Controls.Add(this.loadTestSetButton, 0, 1);
+            this.mainPanel.Controls.Add(this.loadTrainingSetButton, 0, 0);
+            this.mainPanel.Controls.Add(this.computeButton, 1, 3);
+            this.mainPanel.Controls.Add(this.defineLayersPanel, 0, 2);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.RowCount = 4;
+            this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 55F));
+            this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.mainPanel.Size = new System.Drawing.Size(733, 549);
+            this.mainPanel.TabIndex = 2;
+            // 
+            // loadTestSetButton
+            // 
+            this.loadTestSetButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.loadTestSetButton.FlatAppearance.BorderSize = 2;
+            this.loadTestSetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loadTestSetButton.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.loadTestSetButton.Location = new System.Drawing.Point(3, 85);
+            this.loadTestSetButton.Name = "loadTestSetButton";
+            this.loadTestSetButton.Size = new System.Drawing.Size(350, 76);
+            this.loadTestSetButton.TabIndex = 2;
+            this.loadTestSetButton.Text = "Load test set";
+            this.loadTestSetButton.UseVisualStyleBackColor = true;
+            this.loadTestSetButton.Click += new System.EventHandler(this.loadTestSetButton_Click);
+            // 
+            // defineLayersPanel
+            // 
+            this.defineLayersPanel.ColumnCount = 2;
+            this.defineLayersPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.defineLayersPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.defineLayersPanel.Controls.Add(this.deleteLayerButton, 0, 4);
+            this.defineLayersPanel.Controls.Add(this.addLayerButton, 0, 2);
+            this.defineLayersPanel.Controls.Add(this.label2, 0, 1);
+            this.defineLayersPanel.Controls.Add(this.label1, 0, 0);
+            this.defineLayersPanel.Controls.Add(this.numberOfNeurons, 1, 1);
+            this.defineLayersPanel.Controls.Add(this.layersList, 0, 3);
+            this.defineLayersPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.defineLayersPanel.Location = new System.Drawing.Point(3, 167);
+            this.defineLayersPanel.Name = "defineLayersPanel";
+            this.defineLayersPanel.RowCount = 5;
+            this.defineLayersPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.defineLayersPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.defineLayersPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.defineLayersPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.defineLayersPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.defineLayersPanel.Size = new System.Drawing.Size(350, 295);
+            this.defineLayersPanel.TabIndex = 3;
+            // 
+            // deleteLayerButton
+            // 
+            this.defineLayersPanel.SetColumnSpan(this.deleteLayerButton, 2);
+            this.deleteLayerButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.deleteLayerButton.FlatAppearance.BorderSize = 2;
+            this.deleteLayerButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deleteLayerButton.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.deleteLayerButton.Location = new System.Drawing.Point(3, 253);
+            this.deleteLayerButton.Name = "deleteLayerButton";
+            this.deleteLayerButton.Size = new System.Drawing.Size(344, 39);
+            this.deleteLayerButton.TabIndex = 5;
+            this.deleteLayerButton.Text = "Delete layer";
+            this.deleteLayerButton.UseVisualStyleBackColor = true;
+            this.deleteLayerButton.Click += new System.EventHandler(this.deleteLayerButton_Click);
+            // 
+            // addLayerButton
+            // 
+            this.defineLayersPanel.SetColumnSpan(this.addLayerButton, 2);
+            this.addLayerButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.addLayerButton.FlatAppearance.BorderSize = 2;
+            this.addLayerButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addLayerButton.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.addLayerButton.Location = new System.Drawing.Point(3, 91);
+            this.addLayerButton.Name = "addLayerButton";
+            this.addLayerButton.Size = new System.Drawing.Size(344, 38);
+            this.addLayerButton.TabIndex = 3;
+            this.addLayerButton.Text = "Add layer";
+            this.addLayerButton.UseVisualStyleBackColor = true;
+            this.addLayerButton.Click += new System.EventHandler(this.addLayerButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(3, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 59);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "number of neurons:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.defineLayersPanel.SetColumnSpan(this.label1, 2);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Define layers";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numberOfNeurons
+            // 
+            this.numberOfNeurons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numberOfNeurons.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numberOfNeurons.Location = new System.Drawing.Point(185, 39);
+            this.numberOfNeurons.Margin = new System.Windows.Forms.Padding(10);
+            this.numberOfNeurons.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numberOfNeurons.Name = "numberOfNeurons";
+            this.numberOfNeurons.Size = new System.Drawing.Size(155, 26);
+            this.numberOfNeurons.TabIndex = 2;
+            this.numberOfNeurons.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // layersList
+            // 
+            this.defineLayersPanel.SetColumnSpan(this.layersList, 2);
+            this.layersList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layersList.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.layersList.FormattingEnabled = true;
+            this.layersList.ItemHeight = 19;
+            this.layersList.Location = new System.Drawing.Point(3, 135);
+            this.layersList.Name = "layersList";
+            this.layersList.Size = new System.Drawing.Size(344, 112);
+            this.layersList.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(671, 336);
+            this.ClientSize = new System.Drawing.Size(733, 549);
+            this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MLP";
+            this.mainPanel.ResumeLayout(false);
+            this.defineLayersPanel.ResumeLayout(false);
+            this.defineLayersPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfNeurons)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button computeButton;
+        private System.Windows.Forms.Button loadTrainingSetButton;
+        private System.Windows.Forms.TableLayoutPanel mainPanel;
+        private System.Windows.Forms.Button loadTestSetButton;
+        private System.Windows.Forms.TableLayoutPanel defineLayersPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numberOfNeurons;
+        private System.Windows.Forms.Button addLayerButton;
+        private System.Windows.Forms.ListBox layersList;
+        private System.Windows.Forms.Button deleteLayerButton;
     }
 }
 
--- Project1/NeuralNetworks/Form1.cs
+++ Project1/NeuralNetworks/Form1.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿using Encog.Engine.Network.Activation;
+using Encog.ML.Data;
+using Encog.ML.Data.Versatile;
+using Encog.ML.Data.Versatile.Columns;
+using Encog.ML.Data.Versatile.Normalizers.Strategy;
+using Encog.ML.Data.Versatile.Sources;
+using Encog.Neural.Networks;
+using Encog.Neural.Networks.Layers;
+using Encog.Util.CSV;
+using Encog.Util.Simple;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +23,100 @@ namespace NeuralNetworks
 {
     public partial class Form1 : Form
     {
+        private string trainingFileName;
+        private string testFileName;
+        private IMLDataSet trainingSet;
+        private IMLDataSet testSet;
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+        private void loadTrainingSetButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog
+            {
+                Title = @"Open File",
+                Filter = @"CSV files|*.csv",
+            InitialDirectory = Directory.GetCurrentDirectory()
+            };
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                trainingFileName = theDialog.FileName;
+
+                try
+                {
+                    var format = new CSVFormat('.', ',');
+                    trainingSet = EncogUtility.LoadCSV2Memory(trainingFileName, 2, 1, true, format, false);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Wrong file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                } 
+            }
+        }
+
+        private void loadTestSetButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog
+            {
+                Title = @"Open File",
+                Filter = @"CSV files|*.csv",
+                InitialDirectory = Directory.GetCurrentDirectory()
+            };
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                testFileName = theDialog.FileName;
+
+                try
+                {
+                    var format = new CSVFormat('.', ',');
+                    testSet = EncogUtility.LoadCSV2Memory(testFileName, 2, 0, true, format, false);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Wrong file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+        }
+
+        private void addLayerButton_Click(object sender, EventArgs e)
+        {
+            layersList.Items.Add(numberOfNeurons.Value);
+        }
+
+        private void deleteLayerButton_Click(object sender, EventArgs e)
+        {
+            if (layersList.SelectedIndex >= 0)
+            {
+                layersList.Items.RemoveAt(layersList.SelectedIndex);
+            }
+        }
+
+        private void computeButton_Click(object sender, EventArgs e)
+        {
+            BasicNetwork MLP = new BasicNetwork();
+
+            for(int i=0;i<layersList.Items.Count;i++)
+            {
+                int neurons = Int32.Parse(layersList.Items[i].ToString());
+                MLP.AddLayer(new BasicLayer(new ActivationBiPolar(), true, neurons));
+            }
+
+            /*
+            Dla sieci MLP zdefiniowanie
+                liczby warstw, i neuronów ukrytych w każdej warstwie(pełne połączenia pomiędzy warstwami)
+                funkcji aktywacji (unipolarnej lub bipolarnej)
+                obecność biasu
+                liczby iteracji
+                wartości współczynnika nauki
+                wartość współczynnika bezwładności
+                zdefiniowanie rodzaju problemu(klasyfikacja lub regresja) - można zrealizować przez odpowiednie przygotowanie zbioru uczącego i definicję liczby i skali wyjść
+            */
         }
     }
 }
